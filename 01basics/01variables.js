ea211b7bcbb2514ee806ca17ca058ae96e259d4d
Shabaz khan <shabazkhann1221@gmail.com>
2024-10-06 09:36:31 +0000

    variables

---
+++ 01basics/01variables.js
@@ -0,0 +1,13 @@
+const accountId=1221
+let email="shabaz@google.com"
+var pass="12345"
+accountCity="jaipur"
+let accountState
+
+// accountId=1001 
+
+email="sk@google.com"
+pass=2121
+accountCity="bangalore"
+console.table([accountId,accountCity,email,pass,accountState])
+console.log(accountId)
