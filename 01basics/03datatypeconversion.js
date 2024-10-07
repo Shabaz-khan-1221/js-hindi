f7ad5c90767292a0ff848f6fb289506f16eac3c9
Shabaz khan <shabazkhann1221@gmail.com>
2024-10-07 08:48:35 +0000

    conversion of datatypes

---
+++ 01basics/03datatypeconversion.js
@@ -0,0 +1,22 @@
+// let age= true
+// console.log(typeof age)
+
+// let valueinnumber = Number(age)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)
+
+//"33"=> 33
+//"33abc"=>NaN
+//true =>1 false=>0
+let isloggedin="shabaz"
+let booleanisloggedin=Boolean(isloggedin)
+console.log(typeof booleanisloggedin)
+console.log(booleanisloggedin)
+//empty string gives false at the time of conversion
+
+let sumnumber=33
+let stringnumber=String(sumnumber)
+console.log(typeof stringnumber)
+console.log(stringnumber)
+
+
