8dbe55ef67e6e8eb9e20c642d45e63d6977e0722
Shabaz khan <shabazkhann1221@gmail.com>
2024-10-07 09:36:27 +0000

    opeartor updated

--- 01basics/03datatypeconversion.js
+++ 01basics/03datatypeconversion.js
@@ -8,15 +8,43 @@
 //"33"=> 33
 //"33abc"=>NaN
 //true =>1 false=>0
-let isloggedin="shabaz"
-let booleanisloggedin=Boolean(isloggedin)
-console.log(typeof booleanisloggedin)
-console.log(booleanisloggedin)
-//empty string gives false at the time of conversion
+// let isloggedin="shabaz"
+// let booleanisloggedin=Boolean(isloggedin)
+// console.log(typeof booleanisloggedin)
+// console.log(booleanisloggedin)
+// //empty string gives false at the time of conversion
 
-let sumnumber=33
-let stringnumber=String(sumnumber)
-console.log(typeof stringnumber)
-console.log(stringnumber)
+// let sumnumber=33
+// let stringnumber=String(sumnumber)
+// console.log(typeof stringnumber)
+// console.log(stringnumber)
+
+// *************************operations ************************
+
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1="shabaz"
+// let str2="khan"
+// // let str3=str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1+ "2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log(true)
+console.log(+"")
+let num1,num2,num3
+
+let gamecounter=100
+console.log(++gamecounter)
+
+gamecounter++
+console.log(gamecounter)
 
 
