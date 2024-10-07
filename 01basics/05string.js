df48ceac62b15f88ddb33f73c50ed011e8dbce2c
Shabaz khan <shabazkhann1221@gmail.com>
2024-10-07 19:03:40 +0000

    strings

---
+++ 01basics/05string.js
@@ -0,0 +1,33 @@
+// const name="shabaz"
+// const repoCount=50
+
+// console.log(name + repoCount + "value")
+
+// console.log("hello my name is "+ name )
+// console.log('hello my is ${name}');
+// console.log(`hello my name is ${name}`);
+// const newgame= new String('shabaz')
+
+// console.log(newgame[0])
+// console.log(newgame.__proto__)
+// console.log(newgame.length)
+// console.log(newgame.toUpperCase)
+// console.log(newgame)
+// const gameName = new String('shabazkh')
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('k'))
+// const newstring= gameName.substring(0,4)
+// console.log(newstring)
+
+// const anotherstring= gameName.slice(-7,4)
+// console.log(anotherstring)
+const newstring1="      hitesh         "
+console.log(newstring1)
+console.log(newstring1.trim())
+
+const email="https://www.bawaofficial.com/shabaz%khan"
+console.log("before replace => "+ email)
+console.log(email.replace('%','_'))
