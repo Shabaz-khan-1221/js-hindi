9d1a0abe357865aacfba88312ee59e57d50276fc
Shabaz khan <shabazkhann1221@gmail.com>
2024-10-07 09:47:39 +0000

    comparison done

---
+++ 01basics/04comparison.js
@@ -0,0 +1,15 @@
+// console.log(2>1)
+
+console.log("2"> 1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined==0)
+
+//=== triple equal checks accurately
+console.log("2"===2)
+//always try to write clean code
+//thanks to code aur chai
+
