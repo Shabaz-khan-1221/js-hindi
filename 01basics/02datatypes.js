b3c063b831235b71c6955894eddd07ef024fa9ce
Shabaz khan <shabazkhann1221@gmail.com>
2024-10-07 08:23:35 +0000

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //automatic consider it to latest version
+
+//alert(3+3) // we are using nodejs,not browser
+
+console.log(3+3)
+
+console.log("shabaz")
+
+let age=18
+let name="shabaz"
+let islogged=false
+let state
+
+//number  => 2 to power 53
+//bigint
+//string can be used in single as well as double (prefered double quotes)
+//boolean either true or false
+//null standalone value
+//undefined
+//symbol => when we will talk about react (uniqueness)
+
+//object
+
+console.log(typeof "shabaz")
+console.log(typeof null)//object
+console.log(typeof undefined) //undefined
+console.log(typeof age)
+console.log(typeof islogged)
+ 
