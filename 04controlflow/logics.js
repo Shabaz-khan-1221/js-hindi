bb7304c2f321c368aaa064d25e7403c8120cc5e2
Shabaz khan <shabazkhann1221@gmail.com>
2024-10-19 18:03:22 +0000

    conditons

---
+++ 04controlflow/logics.js
@@ -0,0 +1,27 @@
+//if
+
+// if(2==='2'){
+// console.log("system hang")
+// }
+// console.log("maza aaaya")
+// // 2<=2
+// // <,>, <=,>=,==,!=,
+// const temperature=40
+// if(temperature<50){
+//     console.log("yes")
+// }
+const malwablock="moosewala"
+if(malwablock==="sidhu"){
+    console.log("yes")
+
+}
+else
+{
+    console.log("sidhu nhi h yr yeh")
+}
+
+// whenever we write var in code scope becomes global
+//  that will decerease the safety of code
+const balance=1000
+if(balance>=1000)
+    console.log("nhi h hazar se jyada")
